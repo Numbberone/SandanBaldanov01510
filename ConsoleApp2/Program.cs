f636b122cf76b9bbad7041157600c7affc28da65
Dari <bodeeva2001@gmail.com>
2023-09-18 10:18:59 +0800

    name student

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad
+{
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            // 1 student
+             string Name = "Балданов Сандан Батоевич";
+       
+
+              // 2 student
+              string Name2 = "Зубенко Михаил Петрович";
+         
+
+            //ввод с консоли
+          // Console.WriteLine("введите возраст:");
+           // int age1 = int.Parse(Console.ReadLine());
+
+
+            // вывод
+            Console.WriteLine($" Name: {Name} ");
+            Console.WriteLine($" Name: {Name2} ");
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
 
 
